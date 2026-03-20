3485f89dfa237e3a80fd97b9275fd9db17132bc6
yishant11 <yishant11@example.com>
2026-03-20 14:21:05 +0530

    Initial commit

---
+++ blocking.js
@@ -0,0 +1,23 @@
+const crypto = require("crypto");
+
+
+console.log("Hi Ishant!!");
+
+
+// Synchronous function that will block the event loop
+crypto.pbkdf2Sync("password", "salt", 10000000, 512, "sha512");
+console.log("Password encrypted");
+
+// crypto.pbkdf2Sync("password","salt",100,512,"sha512");
+
+// Asynchronous function that will not block the event loop
+crypto.pbkdf2("password", "salt", 1000000, 512, "sha512", () => {
+    console.log("Password encrypted");
+});
+
+function doSomething() {
+    console.log("Doing something...");
+}
+doSomething();
+
+
