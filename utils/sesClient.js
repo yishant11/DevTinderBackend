534ae677cf4e201c2b373d0993cccd78839d1903
yishant11 <yishant11@example.com>
2026-04-29 12:14:05 +0530

    updated ses files

--- utils/sesClient.js
+++ utils/sesClient.js
@@ -1,7 +1,6 @@
 const { SESClient } = require("@aws-sdk/client-ses");
 
-console.log("AWS KEY:", process.env.AWS_ACCESS_KEY);
-console.log("AWS SECRET:", process.env.AWS_SECRET_KEY);
+
 // make sure this is way of passing credentials in v3
 const sesClient = new SESClient({
 
