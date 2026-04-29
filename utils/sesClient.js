7a91cbfec3d7bc6cce12c60ad940d9b27b1e6d97
yishant11 <yishant11@example.com>
2026-04-29 12:38:42 +0530

    env updated

--- utils/sesClient.js
+++ utils/sesClient.js
@@ -4,7 +4,7 @@ const { SESClient } = require("@aws-sdk/client-ses");
 // make sure this is way of passing credentials in v3
 const sesClient = new SESClient({
 
-  region: "ap-south-1",
+  region: process.env.AWS_REGION,
   credentials: {
     accessKeyId: process.env.AWS_ACCESS_KEY,
     secretAccessKey: process.env.AWS_SECRET_KEY,
