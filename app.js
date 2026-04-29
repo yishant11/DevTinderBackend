abbfe5049d7fdb269ecc95330d079ba8ea520598
yishant11 <yishant11@example.com>
2026-04-29 11:28:03 +0530

    dotenv at top level

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+require("dotenv").config();
 // const { auth,user } = require('./middleware/auth');
 const connectDB = require("./config/db");
 const User = require("././src/model/User.js");
